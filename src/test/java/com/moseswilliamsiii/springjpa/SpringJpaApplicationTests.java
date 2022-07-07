62b584f9d7ef0cc1e117f839105a3089126613af
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-07-07 13:06:43 -0400

    First Add

---
+++ src/test/java/com/moseswilliamsiii/springjpa/SpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moseswilliamsiii.springjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
